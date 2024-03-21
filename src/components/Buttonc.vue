4e7162f6a23ca87511b2c35c8243ee93f04c6327
suryanisuc <sucisuryani@student.uir.ac.id>
2024-03-21 15:18:27 +0700

    first commit

---
+++ src/components/Buttonc.vue
@@ -0,0 +1,17 @@
+<!-- adalah sintaks dasar dari vue, harus ada -->
+<template> 
+    <button class="btn">
+        submit
+    </button>
+</template>
+
+<!-- css vue 3 -->
+<!-- scoped berarti style ini hanya berlaku di bagian ini -->
+<style scoped> 
+.btn {
+    background-color: red;
+    color: white;
+    font-size: 28px;
+}
+</style>
+
